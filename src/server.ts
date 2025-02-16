c1634ae7a454bcc618c9a20f05a60677a38ceb07
FernandoLML <fernandolucasmoraesdaluz@gmail.com>
2025-02-16 18:18:05 -0300

    Problema na conexão

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,28 @@
 import express from "express";
 import cors from "cors";
+import { searchInData } from "./search";
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-app.get("/", (req, res) => {
+/*
+app.get("/", (req: Request, res: Response) => {
     res.send("API do MultiSearch funcionando!");
+});*/
+
+app.get("/search", (req: express.Request, res: express.Response ) => {
+    const query = req.query.q as string;
+    if (!query){
+
+     return res.status(400).json({ error: "Query não pode estar vazia"});
+    }
+    const results = searchInData(query);
+    res.json({ results });
 });
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log('Servidor rodando na porta ${PORT}');
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
